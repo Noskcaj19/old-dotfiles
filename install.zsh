ef751d20e3d8fbae02f88fb0a334b39a6f80909a
Noskcaj <kcajdev@gmail.com>
2017-11-15 23:38:57 -0500

    Updated install script for nvim

--- install.zsh
+++ install.zsh
@@ -6,3 +6,10 @@ ln -sf ~/.dotfiles/zsh/zshrc.zsh ~/.zshrc
 
 # Slack Mods
 ln -sf ~/.dotfiles/slack_mods/ ~/.slack_mods
+
+# Neovim
+mkdir -p ~/.config/nvim/
+if [[ -e ~/.config/nvim/init.vim ]]; then
+    mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bk
+fi
+echo "source ~/.dotfiles/vim/init.vim" > ~/.config/nvim/init.vim

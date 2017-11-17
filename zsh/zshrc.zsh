df85f9505795a1c16bf597eaa5dae379307ab129
Noskcaj <kcajdev@gmail.com>
2017-11-17 17:22:22 -0500

    Added some zsh configs

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -3,6 +3,8 @@ if [[ -s "$HOME/.files/zsh/prezto/init.zsh" ]]; then
   source "$HOME/.files/zsh/prezto/init.zsh"
 fi
 
-# more config here
+# Config here
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+for config ($HOME/.files/zsh/configs/*.zsh) source $config
